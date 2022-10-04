d161dd1b909323d71b6a5092a20da8a2e65c8365
Olga Sh <shei-olya@mail.ru>
2022-10-04 15:34:23 +0300

    Проверка элеметов массива по кол-ву символов, определение размера нового массива

--- HM/Program.cs
+++ HM/Program.cs
@@ -8,28 +8,13 @@ for (int i=0; i<n; i++)
 Console.Write("Введите элемент массива: ");
  Array[i]=Console.ReadLine();
 }
-
 Console.WriteLine("Введенный массив: "+string.Join(", ", Array));
 
-/* int count = 0;
+int count = 0;
 for (int i = 0; i < Array.Length; i++)
 {
-    Console.Write($"{Array[i]},");
     if (Array[i].Length < 4)
     {
         count++;
     }
 }
-Console.WriteLine();
-
-string[] Array2 = new string[count];
-int j =0;
-for (int i = 0; i < Array.Length; i++)
-{
-    if (Array[i].Length < 4)
-    {
-        Array2[j]= Array[i];
-        Console.Write($"{Array2[j]},");
-        j++;
-    }
-} */
