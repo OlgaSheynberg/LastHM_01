0b06db897143c24325cd3dbdba6ec97824acbd34
Olga Sh <shei-olya@mail.ru>
2022-10-04 16:49:41 +0300

    Редактирование текста и решения задачи

--- HM/Program.cs
+++ HM/Program.cs
@@ -1,14 +1,13 @@
 ﻿Console.Clear();
 Console.Write("Введите размер массива: ");
-int n= int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
 string[] Array = new string[n];
-
-for (int i=0; i<n; i++)
+for (int i = 0; i < n; i++)
 {
-Console.Write("Введите элемент массива: ");
- Array[i]=Console.ReadLine();
+    Console.Write("Введите элемент массива: ");
+    Array[i] = Console.ReadLine();
 }
-Console.WriteLine("Введенный массив: "+string.Join(", ", Array));
+Console.WriteLine("Введенный массив: " + string.Join(", ", Array));
 
 int count = 0;
 for (int i = 0; i < Array.Length; i++)
@@ -20,14 +19,13 @@ for (int i = 0; i < Array.Length; i++)
 }
 
 string[] Array2 = new string[count];
-
-int j =0;
+int j = 0;
 for (int i = 0; i < Array.Length; i++)
 {
     if (Array[i].Length < 4)
     {
-        Array2[j]= Array[i];
+        Array2[j] = Array[i];
         j++;
     }
 }
-Console.WriteLine("Новый массив: "+string.Join(", ", Array2));
+Console.WriteLine("Новый массив: " + string.Join(", ", Array2));
