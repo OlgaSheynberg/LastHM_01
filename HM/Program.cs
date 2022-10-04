9a19a471f8b9d68378b841c7816c2d3a7cb1c205
Olga Sh <shei-olya@mail.ru>
2022-10-04 15:23:29 +0300

    Заданы параметры для ввода элементов массива с консоли

--- HM/Program.cs
+++ HM/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите размер массива: ");
+int n= int.Parse(Console.ReadLine());
+string[] Array = new string[n];
+
+for (int i=0; i<n; i++)
+{
+Console.Write("Введите элемент массива: ");
+ Array[i]=Console.ReadLine();
+}
+
+Console.WriteLine("Введенный массив: "+string.Join(", ", Array));
+
+/* int count = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($"{Array[i]},");
+    if (Array[i].Length < 4)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+
+string[] Array2 = new string[count];
+int j =0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length < 4)
+    {
+        Array2[j]= Array[i];
+        Console.Write($"{Array2[j]},");
+        j++;
+    }
+} */
